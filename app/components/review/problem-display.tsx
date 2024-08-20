adb22672e77fc1f6768f89e31eed087d08eec10d
Alan Liu <liualan998@gmail.com>
2024-08-19 22:55:55 -0400

    styling on problem display

--- app/components/review/problem-display.tsx
+++ app/components/review/problem-display.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { ProblemData } from '@/app/lib/data-structure';
-import clsx from 'clsx';
 import { useState } from 'react';
 
 export default function ProblemDisplay({ problem }: { problem: ProblemData }){
@@ -18,22 +17,34 @@ export default function ProblemDisplay({ problem }: { problem: ProblemData }){
 
     return (
         <>
-            <div className='space-y-4 pl-2 mb-2'>
-                <h1 className='block'>{problem.title}</h1>
+            <div className='space-y-4 ml-4 pt-3'>
+                <h1 className='block text-xl'><b>{problem.title}</b></h1>
                 <p className='block'>{problem.description}</p>
-                {problem.example && (<p className='block'>{problem.example}</p>)}
+                {problem.example && (
+                    <div className='block pb-3'>
+                        <h3 className='text-lg text-slate-700'><u>Example:</u></h3>
+                        <p>{problem.example}</p>
+                    </div>
+                )}
             </div>
-            <div className='space-y-2 mb-2'>
-                <button className='block p-2 rounded-lg bg-blue-200 hover:bg-blue-400' onClick={handleClickSolution}>Reveal Solution</button>
-                {toggleSolution && 
-                    (<p className='block text-gray-500'>
-                        {problem.solution}
-                    </p>)
-                }
+            <div className='space-y-2 m-4'>
+                <button className='block p-2 rounded-lg bg-slate-100 hover:bg-slate-400' onClick={handleClickSolution}>Reveal Solution</button>
+                {toggleSolution && (
+                    <div className='block h-max'>
+                        <p className='text-gray-500 mb-4'>
+                            {problem.solution}
+                        </p>
+                        <div className='text-right text-sm mr-4'>
+                            <p className='text-slate-600 mb-2'><b>Did you get this question correct?</b></p>
+                            <button className='inline-block w-12 p-2 rounded-md mx-3 bg-slate-100 hover:bg-green-400'>Yes</button>
+                            <button className='inline-block w-12 p-2 rounded-md mx-3 bg-slate-100 hover:bg-red-400'>No</button>
+                        </div>
+                    </div>
+                )}
             </div>
             {problem.notes && (
-                <div className='space-y-2 mb-2'>
-                    <button className='block p-2 rounded-lg bg-blue-200 hover:bg-blue-400' onClick={handleClickNotes}>Expand Notes</button>
+                <div className='space-y-2 m-4'>
+                    <button className='block p-2 rounded-lg bg-slate-100 hover:bg-slate-400' onClick={handleClickNotes}>Expand Notes</button>
                     {toggleNotes && <p className='block text-gray-500'>{problem.notes}</p>}
                 </div>
             )}
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -2,30 +2,31 @@ import { ProblemData } from './data-structure';
 import mysql from 'mysql2/promise';
 import 'dotenv/config';
 
-try {
-    const connection = await mysql.createConnection({
-        host: process.env.SQL_HOST,
-        user: process.env.SQL_USER,
-        password: process.env.SQL_PASSWORD,
-        database: process.env.SQL_DATABASE
-    });
+// try {
+//     const connection = await mysql.createConnection({
+//         host: process.env.SQL_HOST,
+//         user: process.env.SQL_USER,
+//         password: process.env.SQL_PASSWORD,
+//         database: process.env.SQL_DATABASE
+//     });
     
-    console.log('Connected?');
-} catch (err){
-    console.log(err);
-}
+//     console.log('Connected?');
+    
+// } catch (err){
+//     console.log(err);
+// }
 
 
 //Filler function, for now
 export async function fetchProblemById(id: string): Promise<ProblemData>{
     return {
-        title: 'Nothing',
+        title: 'Nothing To See Here',
         id: id,
-        description: 'to',
-        example: 'See',
+        description: 'Lorem Ipsum, dolor sit amet, etc.',
+        example: 'There is no example, but pretend like there is',
         difficulty: 5,
-        solution: 'Here',
-        notes: 'Lol',
+        solution: 'Just imagine the solution exists and is placed right here, even tho it is not',
+        notes: 'There are no notes, but you can pretend like there are',
         lastSeen: new Date(),
         successRate: 1
     };
