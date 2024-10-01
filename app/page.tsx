fe8052e2672ba5b800f433848cda9a89844f8128
Alan Liu <liualan998@gmail.com>
2024-09-30 21:19:33 -0400

    forgot to add redirection on home page lol

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
+import { redirect } from 'next/navigation';
 
 export default async function Home() {
-  
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <p>Let's Try This Again</p>
-      </div>
-    </main>
-  );
+
+  // Redirect to the home page
+  redirect('/home');
 }
